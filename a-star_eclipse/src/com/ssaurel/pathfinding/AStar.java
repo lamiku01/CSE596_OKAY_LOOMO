ed710efc4d3ec02ed419399773693812994ec109
Lauren Mikula <lamiku01@louisville.edu>
2022-03-23 15:48:13 -0400

    Update AStar.java
    bigger grid. 59x75

--- a-star_eclipse/src/com/ssaurel/pathfinding/AStar.java
+++ a-star_eclipse/src/com/ssaurel/pathfinding/AStar.java
@@ -213,7 +213,7 @@ public class AStar {
 	}
 		                
 	public static void main(String[] args) {
-		AStar aStar = new AStar(5,5,0,0,3,2,
+		AStar aStar = new AStar(59,75,0,0,58,74,
 				new int[][] {
 					{0,4}, {2,2}, {3,1}, {3,3}, {2,1}, {2,3}
 				}
